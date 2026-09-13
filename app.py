b6b3fd7b37d39ec74facb3ce06652bca467c758f
SalehaNaseem <salehanaseem515@gmail.com>
2026-09-13 16:15:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ GEMINI_MODEL = get_key("GEMINI_MODEL", "gemini-flash-latest")
 # ============================================
 @st.cache_data
 def load_data():
-    df = pd.read_csv("scholarships_final")
+    df = pd.read_csv("scholarships_final.csv")
     df["amount"] = pd.to_numeric(df["amount"], errors="coerce").fillna(0)
     for c in ["deadline","link","description","location","years"]:
         if c not in df:
